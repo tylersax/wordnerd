826d6dcfeda46b36e20938fe8f273922519d5430
Tyler <tylersax@gmail.com>
2016-03-19 12:50:02 -0700

    fixing broken urls

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,7 +10,10 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^$', hello.views.index, name='index'),
+    url(r'^db', hello.views.db, name='db'),
+    url(r'^admin/', include(admin.site.urls)),
 ]
 
 
