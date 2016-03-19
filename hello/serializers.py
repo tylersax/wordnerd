078b89e58f2291b59ecbfbb4a8c135759bfd048f
Tyler <tylersax@gmail.com>
2016-03-19 13:42:47 -0700

    amending Note

--- hello/serializers.py
+++ hello/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-
+from .models import Note
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
