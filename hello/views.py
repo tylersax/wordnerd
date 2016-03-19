fd53746d748a69acf14397241738999a9c431b3b
Tyler <tylersax@gmail.com>
2016-03-19 13:46:37 -0700

    amending Note

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from .serializers import UserSerializer, GroupSerializer
+from .serializers import UserSerializer, GroupSerializer, NoteSerializer
 from django.shortcuts import render
 from django.http import HttpResponse
 
