06467c13e6ed190893007f405c842d4e418db48a
Tyler Sax <tsax@instagram.com>
2016-09-10 15:25:37 -0700

    reading from dictionarycom rss

--- hello/daily_sweep.py
+++ hello/daily_sweep.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 import utils
+import feedparser
 
-utils.send_message(1046310852095543, 'I am alive, Tyler. And now I\'m going to destroy you.')
+wotd_feed = 'http://www.dictionary.com/wordoftheday/wotd.rss'
+rss = feedparser.parse(wotd_feed)
+wotd = rss.entries[0]['summary'].split(':', 1)[0]
+
+utils.send_message(1046310852095543, 'The word of the day is {wotd}.'.format(wotd=wotd))
--- hello/utils.py
+++ hello/utils.py
@@ -1,7 +1,7 @@
 import requests
 
 def send_message(user, message):
-    access_token = 'EAACogSuZCOdYBAPXIZAuwSZA2o61T0E270JjdT3uAowBbcZCpfXZCuApbb90EtIfJbZBK05j8gLwVLYczUvPAaWS4lp9Ivfnmd2AhDtPUWfaKXYCQGdwqOMJOZCxh1KZCd7vHvpCn1SRipi1ZCmkXXZAsFS3ZBh39Ly4IkXHM8m1cvXQDD4aus61U8d'
+    access_token = 'EAACogSuZCOdYBALENLieXqNBVAgPZCnn7D1F0uviGbTVJTiyPggBqRyszb70aZBSykUFZCJzfNkGsRgplxudzisBQxiUP6D0f6NZC8zQj6FPFDY9pQZC9ni4KcRjMaGghvUgEBFhZC3fnWg1iMWW05rtjeDrif9ZBgMBZCYjfNQZBp8gZDZD'
     url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + access_token
     payload = {'recipient':{'id':user},'message':{'text':message}}
     r = requests.post(url, json=payload)
