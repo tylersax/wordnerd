a8c597ed706df2f16bd9b1625dfc7acd33267304
Tyler Sax <tsax@instagram.com>
2016-09-19 21:11:23 -0700

    fix thumb payload

--- hello/views.py
+++ hello/views.py
@@ -99,7 +99,7 @@ def webhook(request):
                 #this should be in a function
                 existing_wotd = WOTD.objects.filter(word=payload_param)
                 message = '\"{definition}\"'.format(definition=existing_wotd[0].definition)
-                replies = {':thumbs_up_sign:':'null',':thumbs_down_sign:':'null'}
+                replies = {':thumbs_up_sign:':'0.0',':thumbs_down_sign:':'0.0'}
                 utils.send_message_with_replies(psid, message, replies)
 
         # convo = Conversation.create(message_text, user )
