28dc5a1b064219c601de507dec39a811e856437d
Tyler Sax <tsax@instagram.com>
2016-09-19 21:43:38 -0700

    implement typing bubble

--- hello/utils.py
+++ hello/utils.py
@@ -5,12 +5,12 @@ import json
 import emoji
 
 def send_message(user, message):
+    start_typing(user)
     access_token = 'EAACogSuZCOdYBAMQELrH7CpTvbKqx6ckRvJ6VeyiZA3bqCWCvaZAJ8H3wwWeVKTBSbhvkcnzAWZCZCpvpXSljqyzQrSKUJNuVjRsT4WtYMXZCFMyLSLUzNYJE6btdHxZAZB50w9YN81CjJKkwQEIbgTb6VUVNFoMuZCbaNRJaUQjrMmUPMrIZAwxUR'
     url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + access_token
     message_text = emoji.emojize(message)
     payload = {'recipient':{'id':user},'message':{'text':message_text}}
     r = requests.post(url, json=payload)
-    print r.json()
     return r.json()
 
 def send_message_with_replies(user, message, replies):
@@ -20,7 +20,6 @@ def send_message_with_replies(user, message, replies):
     message_text = emoji.emojize(message)
     payload = {'recipient':{'id':user},'message':{'text':message_text,'quick_replies':replies_json}}
     r = requests.post(url, json=payload)
-    print r.json()
     return r.json()
 
 def make_replies_json(replies):
@@ -31,6 +30,13 @@ def make_replies_json(replies):
         replies_json.append(reply_json)
 
     return json.dumps(replies_json)
+
+def start_typing(user):
+    access_token = 'EAACogSuZCOdYBAMQELrH7CpTvbKqx6ckRvJ6VeyiZA3bqCWCvaZAJ8H3wwWeVKTBSbhvkcnzAWZCZCpvpXSljqyzQrSKUJNuVjRsT4WtYMXZCFMyLSLUzNYJE6btdHxZAZB50w9YN81CjJKkwQEIbgTb6VUVNFoMuZCbaNRJaUQjrMmUPMrIZAwxUR'
+    url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + access_token
+    payload = {'recipient':{'id':user},'sender_action':'typing_on'}
+    r = requests.post(url, json=payload)
+    return r.json()
 
 def get_definition(word):
     key = 'be629e6642b73b9c3b4710360ec0a0d680d43628b4ba9e162'
