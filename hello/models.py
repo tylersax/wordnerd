4a274a975f21e3f1374e95a2606013b9dd844dfa
Tyler Sax <tsax@SkyNet.local>
2017-08-05 20:19:48 -0700

    field error 4

--- hello/models.py
+++ hello/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 from datetime import datetime
 from django.contrib.auth.models import User, Group
 import logging
