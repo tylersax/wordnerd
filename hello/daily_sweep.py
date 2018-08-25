6233673a6fc2d617abae3ea587e1b0a00cba4546
Tyler Sax <tsax@SkyNet.local>
2018-08-25 19:03:22 +0300

    do not message on weekends

--- hello/daily_sweep.py
+++ hello/daily_sweep.py
@@ -12,11 +12,14 @@ import hello.utils as utils
 import feedparser
 from hello.models import WOTD, FBUser
 
+import datetime
+
 wotd_feed = 'https://wordsmith.org/awad/rss1.xml'
 rss = feedparser.parse(wotd_feed)
 wotd_string = rss.entries[0]['title']
 
 existing_wotd = WOTD.objects.filter(word=wotd_string)
+
 if len(existing_wotd) > 1:
     wotd = existing_wotd[0]
 else:
@@ -29,8 +32,14 @@ else:
     )
     wotd.save()
 
-users = FBUser.objects.filter(subscribed=True)
-for user in users:
-    message = 'The word of the day is \"{wotd}.\"'.format(wotd=wotd.word)
-    replies = {'Definition, please?':'define.{wotd}'.format(wotd=wotd.word)}
-    utils.send_message_with_replies(user.psid, message, replies)
+# This hours adjustment will be wrong when daylight savings changes
+# Note that weekday() returns Mon-Sun as [0,6]
+pst_day_of_week = (datetime.datetime.now() - datetime.timedelta(hours=7)).weekday()
+
+# Only message users on weekdays
+if pst_day_of_week not in (5,6):
+    users = FBUser.objects.filter(subscribed=True)
+    for user in users:
+        message = 'The word of the day is \"{wotd}.\"'.format(wotd=wotd.word)
+        replies = {'Definition, please?':'define.{wotd}'.format(wotd=wotd.word)}
+        utils.send_message_with_replies(user.psid, message, replies)
